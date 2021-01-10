93c9014ab001185ff2a79223410daabd65ef6179
nikunj goyal <nikunjgoyal31@gmail.com>
2021-01-10 20:04:17 +0530

    Sixth Test case fails

--- StringCalculator_TDD/src/TestCalculate.java
+++ StringCalculator_TDD/src/TestCalculate.java
@@ -10,25 +10,36 @@ public class TestCalculate {
 		calculator=new Calculator();
 	}
 	
-	public void emptyStringReturnsZero() {
+	//First Test Case
+	public void emptyStringReturnsZero() {						
 		assertEquals(calculator.calculate(""), 0);
 		
 	}
 	
+	//Second Test Case
 	public void singleValueIsReplied() {
 		assertEquals(calculator.calculate("1"), 1);
 		
 	}
 	
+	//Third Test Case
 	public void twoNumbersCommaDelimitersReturnSum() {
 		assertEquals(calculator.calculate("1,2"),3);
 	}
 	
+	//Fourth Test Case
 	public void twoNumbersNewLineDelimitersReturnSum() {
 		assertEquals(calculator.calculate("1\n2"),3);
 	}
 	
+	//Fifth Test Case
 	public void threeNumbersCommaDelimitersBothWaysReturnSum() {
 		assertEquals(calculator.calculate("1,2,3"),6);
+	}
+	
+	//Sixth Test case
+	@Test(expectedExceptions=Exception.class)
+	public void negativeInputReturnsException() {
+		calculator.calculate("-1");
 	}
 }
